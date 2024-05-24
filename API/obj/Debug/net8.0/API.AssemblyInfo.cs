f6e2f193ac1b2b24ce3f50362da99c9c62e09c96
FriptuT <friptut@gmail.com>
2024-05-24 13:54:30 +0300

    preparation for extending the app

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869cc76e8a3684996110802b2610d826eb313cde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0732762aa1a01c35f78aa855259bf27461ec5281")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
